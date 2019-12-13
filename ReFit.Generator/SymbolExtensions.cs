79d1cec5703bb9b4c6a793d1e82362547307b440
Jérôme Laban <jerome.laban@nventive.com>
2019-12-12 23:18:26 -0500

    Add basic source generation for interface stubs

---
+++ ReFit.Generator/SymbolExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.CodeAnalysis;
+
+namespace ReFit.Generator
+{
+    internal static class SymbolExtensions
+    {
+        public static IEnumerable<IMethodSymbol> GetAllInterfaceMethods(this INamedTypeSymbol symbol)
+        {
+            foreach(var member in symbol.GetAllInterfaces(includeCurrent: false).SelectMany(GetAllInterfaceMethods))
+            {
+                yield return member;
+            }
+
+            foreach (var member in symbol.GetMethods())
+            {
+                yield return member;
+            }
+        }
+    }
+}
